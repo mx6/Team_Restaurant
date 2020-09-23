0a7fc77146cc5357803273d4c96f34697fdb5314
miadehaan <64979457+miadehaan@users.noreply.github.com>
2020-09-22 19:17:18 -0700

    Server created use Express on port 3000

---
+++ server.js
@@ -0,0 +1,26 @@
+// Require/import the HTTP module
+// hhtp (AKA webServer)
+var http = require("http");
+
+// Define a port to listen for incoming requests
+// http port 80
+// https port 443
+var PORT = 8080;
+
+// Create a generic function to handle requests and responses
+function handleRequest(request, response) {
+
+  // Send the below string to the client when the user visits the PORT URL
+  response.end("It Works!! Path Hit: " + request.url);
+}
+
+// Use the Node HTTP package to create our server.
+// Pass the handleRequest function to empower it with functionality.
+var server = http.createServer(handleRequest); // 'handleRequest' = the callback
+
+// Start our server so that it can begin listening to client requests.
+server.listen(PORT, function() {
+
+  // Log (server-side) when our server has started
+  console.log("Mia's web server is listening on: http://localhost:" + PORT);
+});
